3b33630fd67a7758485aee3c4fa9c1679d7aa192
adnanmulalic <adnan.mulalic3@gmail.com>
2022-08-28 11:59:23 +0200

    wip

--- javascript.js
+++ javascript.js
@@ -5,18 +5,12 @@ for (let i = 0; i < 16; i++) {
     bigDiv.appendChild(smallDiv);
 }
 
+function turnBlack() {
+    this.classList.add('small-div-clicked');
+}
+
 const smallDivs = document.querySelectorAll(".small-div");
 
 smallDivs.forEach(element => {
-    let mouseDown = 0;
-    element.addEventListener("mousedown", () => {
-        mouseDown = 1;
-        element.classList.add('small-div-clicked');
-    })
-
-    if (element.className === "small-div small-div-clicked") {
-        element.addEventListener("mouseover", () => {
-            element.classList.add('small-div-clicked');
-            })   
-    }
+    element.addEventListener("mouseover", turnBlack);
 });
