333cf2c0f5b10c9feb30d4b6ade6c2a902686ea3
adnanmulalic <adnan.mulalic3@gmail.com>
2022-08-30 00:47:27 +0200

    wip

--- javascript.js
+++ javascript.js
@@ -1,14 +1,29 @@
 const bigDiv = document.querySelector('#big-div');
-const selectedSquares = document.querySelector('#squares');
+const selectedSquares = document.querySelectorAll('.squares');
 const changer = document.querySelector('#changer');
-console.log(changer.getAttribute("value"));
+const button = document.querySelector('#button');
+let squareSide = 4;
 
+changer.addEventListener("change", ()=> {
+    squareSide = parseInt(changer.value);
+    selectedSquares.forEach(element => {
+        element.innerText = changer.value;
+    });
+});
 
-for (let i = 0; i < 8; i++) {
-    const smallDiv = document.createElement('div');
-    smallDiv.classList.add('small-div');
-    bigDiv.appendChild(smallDiv);
-}
+button.addEventListener("click", ()=> {
+    while (bigDiv.firstChild) {
+        bigDiv.removeChild(bigDiv.firstChild);
+    }
+    for (let i = 0; i < changer.value; i++) {
+        const smallDiv = document.createElement('div');
+        smallDiv.classList.add('small-div');
+        squareSide = 720 / parseInt(changer.value)
+        smallDiv.style.minWidth = `${squareSide}px`;
+        smallDiv.style.height = `${squareSide}px`;
+        bigDiv.appendChild(smallDiv);
+    }
+})
 
 function turnBlack() {
     this.classList.add('small-div-clicked');
