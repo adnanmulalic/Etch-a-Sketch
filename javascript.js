0a11bc040e0b54f9bd12b11ad0bdd02acce416a3
adnanmulalic <adnan.mulalic3@gmail.com>
2022-08-30 11:54:06 +0200

    Event listeners and square creating done, minor positioning and  styling needed

--- javascript.js
+++ javascript.js
@@ -4,17 +4,12 @@ const changer = document.querySelector('#changer');
 const button = document.querySelector('#button');
 let squareSide = 4;
 
-changer.addEventListener("change", ()=> {
-    squareSide = parseInt(changer.value);
-    selectedSquares.forEach(element => {
-        element.innerText = changer.value;
-    });
-});
+function turnBlack() {
+    this.classList.add('small-div-clicked');
+}
 
-button.addEventListener("click", ()=> {
-    while (bigDiv.firstChild) {
-        bigDiv.removeChild(bigDiv.firstChild);
-    }
+//runs on page load -->
+for (let y = 0; y < changer.value; y++) {
     for (let i = 0; i < changer.value; i++) {
         const smallDiv = document.createElement('div');
         smallDiv.classList.add('small-div');
@@ -23,14 +18,39 @@ button.addEventListener("click", ()=> {
         smallDiv.style.height = `${squareSide}px`;
         bigDiv.appendChild(smallDiv);
     }
-})
-
-function turnBlack() {
-    this.classList.add('small-div-clicked');
 }
-
-const smallDivs = document.querySelectorAll(".small-div");
-
+let smallDivs = document.querySelectorAll(".small-div");
 smallDivs.forEach(element => {
     element.addEventListener("mouseover", turnBlack);
 });
+//runs on page load <--
+
+// visually changes number of squares chosen
+changer.addEventListener("change", ()=> {
+    squareSide = parseInt(changer.value);
+    selectedSquares.forEach(element => {
+        element.innerText = changer.value;
+    });
+});
+
+//listeners that remove, recreate squares and changes their color
+button.addEventListener("click", ()=> {
+    while (bigDiv.firstChild) {
+        bigDiv.removeChild(bigDiv.firstChild);
+    }
+    for (let y = 0; y < changer.value; y++) {
+        for (let i = 0; i < changer.value; i++) {
+            const smallDiv = document.createElement('div');
+            smallDiv.classList.add('small-div');
+            squareSide = 720 / parseInt(changer.value);
+            smallDiv.style.minWidth = `${squareSide}px`;
+            smallDiv.style.height = `${squareSide}px`;
+            bigDiv.appendChild(smallDiv);
+        }
+    }
+    smallDivs = document.querySelectorAll(".small-div");
+    smallDivs.forEach(element => {
+        element.addEventListener("mouseover", turnBlack);
+    });
+});
+
